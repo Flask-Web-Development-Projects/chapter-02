7a67059be57af45e0a81667c7cf8ff670f20a011
nhuntwalker <nhuntwalker@Gmail.com>
2019-05-11 16:35:35 -0700

    Set up some structure for the user model

--- server/forum/__init__.py
+++ server/forum/__init__.py
@@ -1,3 +1,4 @@
 from flask_api import FlaskAPI
 
+
 app = FlaskAPI(__name__)
--- server/setup.py
+++ server/setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
 required = [
-    "flask-api"
+    "flask-api==1.1",
+    "flask-migrate==2.4.0",
+    "flask-script==2.0.6",
+    "flask-sqlalchemy==2.4.0",
+    "psycopg2==2.8.2",
 ]
 
 setup(
