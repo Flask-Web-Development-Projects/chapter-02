8aec9f9299143cff0208be98a04433bbda58e62a
nhuntwalker <nhuntwalker@Gmail.com>
2019-05-11 16:39:05 -0700

    Created a migrations directory and the manage.py file

--- server/forum/__init__.py
+++ server/forum/__init__.py
@@ -1,4 +1,12 @@
+import os
 from flask_api import FlaskAPI
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = FlaskAPI(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'POSTGRES_URL',
+    'postgres://localhost:5432/forumdb'
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
