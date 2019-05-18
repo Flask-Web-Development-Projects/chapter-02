ae40aa37315e095942dfcb421a190b13a5c0e1ba
nhuntwalker <nhuntwalker@Gmail.com>
2019-05-18 08:16:46 -0700

    Updated the user model to add a user token, created the get_user function, added the verify_token function to auth.py and corrected the authenticate function to include the new token scheme

--- server/forum/auth.py
+++ server/forum/auth.py
@@ -1,13 +1,13 @@
 from flask import Response
 from flask_httpauth import HTTPTokenAuth
-from passlib.hash import pbkdf2_sha256 as hasher
+from forum.users.models import User, get_user
 
 auth = HTTPTokenAuth(scheme="Token")
 
-def authenticate(response: Response, username: str) -> bool:
+def authenticate(response: Response, user: User) -> Response:
     """Authenticate an outgoing response with the user's token.
     
-    Hash the user's username and set it as the authentication token
+    Set the user's username and authentication token
     on an outgoing response.
 
     Parameters
@@ -24,6 +24,16 @@ def authenticate(response: Response, username: str) -> bool:
     Response
         The full response object, now with a cookie
     """
-    token = hasher.hash(username)
+    token = f"{user.username}:{user.token}"
     response.set_cookie('auth_token', value=token)
     return response
+
+
+@auth.verify_token
+def verify_token(token: str) -> bool:
+    """Verify that the incoming request has the expected token."""
+    if token:
+        username = token.split(':')
+        user = get_user(username)
+        return token == user.token
+    return False
--- server/forum/users/models.py
+++ server/forum/users/models.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Optional
 
 from passlib.hash import pbkdf2_sha256 as hasher
 from sqlalchemy import Column, DateTime, Integer, Text, Unicode
@@ -34,8 +35,31 @@ class User(db.Model):
     creation_date = Column(DateTime, default=datetime.datetime.utcnow)
     last_updated = Column(DateTime, default=datetime.datetime.utcnow)
     bio = Column(Text)
+    token = Column(Unicode)
 
     def __init__(self, *args, **kwargs) -> None:
         """Overrides the inherited init method to hash incoming passwords."""
         kwargs["password"] = hasher.hash(kwargs["password"])
+        kwargs["token"] = hasher.hash(kwargs["username"])
         super().__init__(self, *args, **kwargs)
+
+
+def get_user(username: str) -> Optional[User]:
+    """Retrieves a user object if it exists.
+
+    When passed username this function checks the database to return
+    the corresponding user object. If that user doesn't exist,
+    return None
+
+    Parameters
+    ----------
+    username : str
+        The username for a given user.
+
+    Returns
+    -------
+    User, None
+        If the user with that username exists, the user is returned.
+        Otherwise, None is returned.
+    """
+    return User.query.filter_by(username=username).first()
