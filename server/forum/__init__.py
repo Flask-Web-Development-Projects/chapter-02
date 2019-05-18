3e094ad1e427c64879a64630937f3c360bbfb7dc
nhuntwalker <nhuntwalker@Gmail.com>
2019-05-18 08:22:50 -0700

    Switched out Flask API for Flask RESTplus because the latter works better with Blueprints

--- server/forum/__init__.py
+++ server/forum/__init__.py
@@ -1,8 +1,8 @@
 import os
-from flask_api import FlaskAPI
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = FlaskAPI(__name__)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'POSTGRES_URL',
     'postgres://localhost:5432/forumdb'
