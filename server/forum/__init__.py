0ed35ff3316068e0bab1b6ce72c4eac14a744c95
nhuntwalker <nhuntwalker@Gmail.com>
2019-05-18 09:00:05 -0700

    Decided I wanted to use FlaskAPI afterall. Also reordered an import so that I dont get circular imports later on

--- server/forum/__init__.py
+++ server/forum/__init__.py
@@ -1,17 +1,17 @@
 import os
-from flask import Flask
+from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 
-from forum.users.routes import user_routes
-
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'POSTGRES_URL',
     'postgres://localhost:5432/forumdb'
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+from forum.users.routes import user_routes
+
 API_PREFIX = '/api/v1'
 app.register_blueprint(user_routes, url_prefix=API_PREFIX)
-
-db = SQLAlchemy(app)
