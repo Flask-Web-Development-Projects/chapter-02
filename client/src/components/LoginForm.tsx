15e50ea48d5847a0d2ab385c6f8a9e447133c3b8
nhuntwalker <nhuntwalker@Gmail.com>
2019-05-25 02:50:32 -0700

    Created the LoginForm component. Now need to tie it in as a modal to the App

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,49 @@
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+
+
+export const LoginForm = () => {
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ rememberMe, setRememberMe ] = useState(false);
+
+    const updateUsername = (event: ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    }
+    const updatePassword = (event: ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    }
+    const updateRememberMe = (event: ChangeEvent<HTMLInputElement>) => {
+        setRememberMe(event.target.checked);
+    }
+    const submitLogin = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setPassword('');
+    }
+
+    return <form onSubmit={ submitLogin }>
+        <div>
+            <label htmlFor="username">Username:</label>
+            <input
+                type="text" name="username"
+                value={ username } onChange={ updateUsername }
+            />
+        </div>
+        <div>
+            <label htmlFor="password">Password:</label>
+            <input
+                type="password" name="password"
+                value={ password } onChange={ updatePassword }
+            />
+        </div>
+        <div>
+            <input
+                type="checkbox" name="remember-me"
+                checked={ rememberMe } onChange={ updateRememberMe }
+            />
+            <label htmlFor="remember-me">Remember me</label>
+        </div>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+}
