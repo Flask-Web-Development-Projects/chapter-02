3948528d5585d386d9e20722af209033e631298e
nhuntwalker <nhuntwalker@Gmail.com>
2019-05-28 01:35:37 -0700

    Updated the AuthUserDetail component to start updating the username

--- client/src/components/AuthUserDetail.tsx
+++ client/src/components/AuthUserDetail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 
 import { PostTitleList } from './PostTitleList';
 import { User } from '../types';
@@ -9,11 +9,29 @@ interface Props {
 }
 
 export const AuthUserDetail = ({ authUser }: Props) => {
+  const [ username, setUsername ] = useState(authUser.username);
+  const [ editingUsername, setUsernameEdit ] = useState(false);
+
+  const updateUsername = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  }
+
   return <article>
-    <h2>User: { authUser.username }</h2>
+    <h2>
+      {
+        editingUsername ?
+        `User: ${authUser.username} ${<button onClick={() => setUsernameEdit(true)}>Edit</button>}` :
+        `Username: ${<input
+          type="text" name="username"
+          value={ username } onChange={ updateUsername }
+        />}`
+      }
+    </h2>
     <p>Account Created: {resolveTime(authUser.creation_date)}</p>
     <p>Last Updated: {resolveTime(authUser.last_updated)}</p>
-    {authUser.bio !== '' ? authUser.bio : null}
+    <section>
+      Bio: {authUser.bio !== '' ? authUser.bio : '--'}
+    </section>
     <h3>Most Recent Posts</h3>
     <PostTitleList posts={authUser.posts} />
   </article>
